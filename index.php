da092bc46cd5869c4285db7cb117038f361e7d9c
Alaa <Alaa_falah@outlook.de>
2018-11-05 13:58:55 +0100

    added dompdf and created pdf export option

--- index.php
+++ index.php
@@ -1,8 +1,112 @@
 <?php
 
+// include autoloader
+require_once __DIR__ . '/vendor/autoload.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+
+
+
 $user = 75;
 $meinearbeit = [];
 $kw = [];
+$text = [];
+$beispiel = [
+
+    [
+        'bereich' => '1.1. Informations- u. telekommunikationstechnische Produkte u. Märkte',
+        'zeit' =>',',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => '1.2 Herstellen und Betreuen von Systemlösungen',
+        'zeit' =>'3 – 4 Monate',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => '2.1. Geschäfts- und Leistungsprozesse',
+        'zeit' =>'2 - 4 Monate',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => '3.1. Geschäfts- und Leistungsprozesse',
+        'zeit' =>',',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => '3.2. Herstellen und Betreuen von Systemlösungen',
+        'zeit' =>',',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => 'Systementwicklung',
+        'zeit' =>'3 – 5 Monate ',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => '3.1. Geschäfts- und Leistungsprozesse',
+        'zeit' =>',',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => '3.2. Herstellen und Betreuen von Systemlösungen',
+        'zeit' =>',',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => 'Systementwicklung',
+        'zeit' =>'3 – 5 Monate ',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => 'Systementwicklung',
+        'zeit' =>'3 – 5 Monate ',
+        'vonbis' =>'10-20'
+    ],
+
+    [
+        'bereich' => 'Systementwicklung',
+        'zeit' =>'3 – 5 Monate ',
+        'vonbis' =>'10-20'
+    ],
+
+
+
+];
+
+$template_table = '<tr>
+                <td width="40%">{{$bereich}}</td>
+                <td width="20%">{{$zeit}}</td>
+				<td width="40%">{{$vonbis}}</td>
+            </tr>';
+
+$bla = '';
+
+foreach ($beispiel as $value)
+{
+
+    $final = str_replace('{{$bereich}}', $value ['bereich'],$template_table ) ;
+    $final = str_replace('{{$zeit}}', $value ['zeit'],$final) ;
+    $final = str_replace('{{$vonbis}}', $value ['vonbis'],$final ) ;
+    $bla .= $final;
+
+
+
+}
+
+
+//echo ($zeile['bereich']); die;
 
 // Config
 setlocale(LC_ALL, 'de_DE@euro');
@@ -17,7 +121,7 @@ foreach ($pdo->query($sql) as $row) {
    $kw = $dateTime->format("W");
    $tag = $dateTime->format("d.m.Y");
 
-   $meinearbeit[$kw][$tag] = $dateTime->format("l").': '.$row['task_description'];
+   $meinearbeit[$kw][$tag] = $dateTime->format("").' '.$row['task_description'];
 }
 
 
@@ -43,11 +147,34 @@ foreach ($meinearbeit as $kw=>&$data) {
     ksort($data);
 }
 
+
+
 reset($meinearbeit);
 $allererste_woche = key($meinearbeit);
 
+reset($meinearbeit[$allererste_woche]);
+$montag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+next($meinearbeit[$allererste_woche]);
 
-print_r($meinearbeit[$allererste_woche]);
+$dienstag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+next($meinearbeit[$allererste_woche]);
+
+$mittwoch = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+next($meinearbeit[$allererste_woche]);
+
+$donnerstag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+next($meinearbeit[$allererste_woche]);
+
+$freitag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+next($meinearbeit[$allererste_woche]);
+
+$samstag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+next($meinearbeit[$allererste_woche]);
+
+$sonntag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+next($meinearbeit[$allererste_woche]);
+
+
 
 /*
 
@@ -78,7 +205,7 @@ $meinearbeit[$kw][$tag] = "task";
 
 
 
-*/
+/**/
 
 
 
@@ -101,7 +228,17 @@ $variablen = [
     'betrieb'=>'itspoon GmbH',
     'ausbilder'=>'Margus Kohv',
     'ende'=>'2021',
-	'jahr'=>'1'
+	'jahr'=>'1',
+    'erster_tag'=>$first_day_in_array,
+    'letzter_tag'=>$check_day,
+    'montag'=> $montag,
+    'dienstag'=> $dienstag,
+    'mittwoch'=> $mittwoch,
+    'donnerstag'=> $donnerstag,
+    'freitag'=> $freitag,
+    'samstag'=> $samstag,
+    'sonntag'=> $sonntag,
+    'bla' => $bla
 ];
 
 // ---------------- DON'T MESS WITH THE CODE BELOW UNLESS YOU ARE A PROGRAMMING GOOF!
@@ -112,4 +249,20 @@ foreach ($variablen as $key=>$val) {
 	$templateContents = str_replace('{{ '.$key.' }}', $val, $templateContents);
 }
 
-echo $templateContents;
+if (empty($_GET['pdf'])) {
+    echo $templateContents;
+} else {
+    // instantiate and use the dompdf class
+    $dompdf = new Dompdf();
+    $dompdf->loadHtml($templateContents);
+
+    // (Optional) Setup the paper size and orientation
+    $dompdf->setPaper('A4', 'portrait');
+
+    // Render the HTML as PDF
+    $dompdf->render();
+
+    // Output the generated PDF to Browser
+    $dompdf->stream();
+    // PDF (https://github.com/dompdf/dompdf)
+}
---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+
+// include autoloader
+require_once __DIR__ . '/vendor/autoload.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml(file_get_contents ('Nachweis2.html'));
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
+?>
