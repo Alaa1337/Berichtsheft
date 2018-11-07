418230089d4f6b20ab6efac91b89375aa728a138
Alaa <Alaa_falah@outlook.de>
2018-11-07 18:06:16 +0100

    pdf breaks added

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ use Carbon\Carbon;
 
 $user = 75;
 $meinearbeit = [];
-$kw = [];
 $text = [];
 $beispiel = [
 
@@ -158,6 +157,8 @@ foreach ($prepared->fetchAll() as $row) {
 }
 
 
+
+
 foreach ($meinearbeit as $kw=>$data) {
     if (count($data) != 7) {
         reset($data);
@@ -190,11 +191,10 @@ $templates = [
 	3=>__DIR__.'/Nachweis3.html',
 ];
 
+
+
 // Unsere Auswahl
 $template = $templates[$_GET['template']];
-
-
-
 
 
 
@@ -202,77 +202,97 @@ $template = $templates[$_GET['template']];
 // DO NOT TOUCH A FUCKING THING ABOVE THIS LINE
 // ===============================================================================
 
+$c = 0;
+
+foreach ($meinearbeit as $allererste_woche=>$data) {
+
+    $c++;
+
+    //$pagebreak = echo '</html>';
+    $letztewoche = $kw;
+
+    reset($meinearbeit[$allererste_woche]);
+    $montag_key = key($meinearbeit[$allererste_woche]);
+    $montag = $meinearbeit[$allererste_woche][$montag_key];
+    next($meinearbeit[$allererste_woche]);
+
+    $dienstag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+    next($meinearbeit[$allererste_woche]);
+
+    $mittwoch = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+    next($meinearbeit[$allererste_woche]);
+
+    $donnerstag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+    next($meinearbeit[$allererste_woche]);
+
+    $freitag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+    next($meinearbeit[$allererste_woche]);
+
+    $samstag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
+    next($meinearbeit[$allererste_woche]);
+
+    $sonntag_key = key($meinearbeit[$allererste_woche]);
+    $sonntag = $meinearbeit[$allererste_woche][$sonntag_key];
+    next($meinearbeit[$allererste_woche]);
+
+    $html = file_get_contents($template);
+    $variablen = [
+    	'heftnr'=>'1',
+    	'vorname'=>'Alaa',
+    	'nachname'=>'Falah',
+        'adresse'=>'Salzstraße 18',
+        'beruf'=>'Fachinformatiker, Anwendungsentwicklung',
+        'betrieb'=>'itspoon GmbH',
+        'ausbilder'=>'Margus Kohv',
+        'ende'=>'2021',
+    	'jahr'=>'1',
+        'erster_tag'=>$montag_key,
+        'letzter_tag'=>$sonntag_key,
+        'montag'=> $montag,
+        'dienstag'=> $dienstag,
+        'mittwoch'=> $mittwoch,
+        'donnerstag'=> $donnerstag,
+        'freitag'=> $freitag,
+        'samstag'=> $samstag,
+        'sonntag'=> $sonntag,
+        'pagebreak' => $pagebreak
+    ];
 
 
+    if ($c >= count($meinearbeit)) {
+        $variablen['pagebreak'] = "";
+    } else {
+        $variablen['pagebreak'] = "<div style=\"page-break-after: always;\">";
+    }
 
 
+    foreach ($variablen as $key=>$val) {
+    	$html = str_replace('{{ '.$key.' }}', $val, $html);
 
 
-reset($meinearbeit);
-$allererste_woche = key($meinearbeit);
+    }
 
-if (null !== $allererste_woche) {
+//print_r(key($meinearbeit));die;
 
-reset($meinearbeit[$allererste_woche]);
-$montag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
-next($meinearbeit[$allererste_woche]);
+    $templateContents .= $html;
 
-$dienstag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
-next($meinearbeit[$allererste_woche]);
 
-$mittwoch = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
-next($meinearbeit[$allererste_woche]);
-
-$donnerstag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
-next($meinearbeit[$allererste_woche]);
-
-$freitag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
-next($meinearbeit[$allererste_woche]);
-
-$samstag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
-next($meinearbeit[$allererste_woche]);
-
-$sonntag = $meinearbeit[$allererste_woche][key($meinearbeit[$allererste_woche])];
-next($meinearbeit[$allererste_woche]);
 }
 
-$variablen = [
-	'heftnr'=>'1',
-	'vorname'=>'Alaa',
-	'nachname'=>'Falah',
-    'adresse'=>'Salzstraße 18',
-    'beruf'=>'Fachinformatiker, Anwendungsentwicklung',
-    'betrieb'=>'itspoon GmbH',
-    'ausbilder'=>'Margus Kohv',
-    'ende'=>'2021',
-	'jahr'=>'1',
-    'erster_tag'=>$vom,
-    'letzter_tag'=>$bis,
-    'montag'=> $montag,
-    'dienstag'=> $dienstag,
-    'mittwoch'=> $mittwoch,
-    'donnerstag'=> $donnerstag,
-    'freitag'=> $freitag,
-    'samstag'=> $samstag,
-    'sonntag'=> $sonntag,
-    'bla' => $bla
-];
 
 
 
 
-$templateContents = file_get_contents($template);
 
-foreach ($variablen as $key=>$val) {
-	$templateContents = str_replace('{{ '.$key.' }}', $val, $templateContents);
+$templateContents .= "</html>";
+
+
+if (key($meinearbeit) == $letztewoche )
+{
+
+    //$pagebreak = "nomraler text";
+    echo $variablen['pagebreak'];
 }
-
-
-
-
-
-
-
 
 // ===============================================================================
 // DO NOT TOUCH A FUCKING THING BELOW THIS LINE
